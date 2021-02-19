503d81d75a74052f5ac7056a1279fec984aec792
RaeLin <haiwen1206@gmail.com>
2021-02-20 00:24:09 +0800

    no message

---
+++ build/precache-manifest.830fa354a547f97a9cae204d75a20c4a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db013a32f0e0131671ec124ce912335b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f892db0a5f983861fec",
+    "url": "/static/css/main.486f1c86.chunk.css"
+  },
+  {
+    "revision": "cefbccf25aa78427d3db",
+    "url": "/static/js/2.275fce10.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/static/js/2.275fce10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f892db0a5f983861fec",
+    "url": "/static/js/main.e8939019.chunk.js"
+  },
+  {
+    "revision": "4973c69714384021b607",
+    "url": "/static/js/runtime-main.f98c8953.js"
+  },
+  {
+    "revision": "4748c0e84bc81716f8163ec6a5976e58",
+    "url": "/static/media/Food.4748c0e8.webp"
+  },
+  {
+    "revision": "00d4f489fbd765e20e7c494afec2887e",
+    "url": "/static/media/airplane.00d4f489.jpg"
+  },
+  {
+    "revision": "f502701531141d3de7689c8bce8264e9",
+    "url": "/static/media/arrow.f5027015.svg"
+  },
+  {
+    "revision": "937431c68be9d2525f704224bbf71e0c",
+    "url": "/static/media/bus.937431c6.jpeg"
+  },
+  {
+    "revision": "f3ae220f6ce9a956d7244a018bbe40f8",
+    "url": "/static/media/hotel.f3ae220f.png"
+  },
+  {
+    "revision": "af101c31a090a76df78fd39b2cbddf72",
+    "url": "/static/media/petra.af101c31.jpg"
+  },
+  {
+    "revision": "2b873f98805403a13b646218ac3c4489",
+    "url": "/static/media/petraSmall.2b873f98.png"
+  },
+  {
+    "revision": "b3731c68406b43847d2b66b872d0b45c",
+    "url": "/static/media/train.b3731c68.jpg"
+  }
+]);
